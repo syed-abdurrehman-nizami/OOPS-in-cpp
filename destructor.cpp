c1f5d150e6792bcdf5a4b90ddf98034cbc779d53
Syed Abdurrehman Nizami <syedabdurrehman859@gmail.com>
2024-03-10 20:00:21 +0500

    initial commit

--- destructor.cpp
+++ destructor.cpp
@@ -1,57 +1,65 @@
 // In this program i make Destructor whcich is called at end of program and destory all objects.
 // Destructor is called 5 times because there are 5 objects in this program.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-class worker{
+class worker
+{
     string name;
 
-    public:
-
+public:
     int age;
     int salary;
 
-    worker ()  {
-        cout<<endl<<"Default constructor called"<<endl;
+    worker()
+    {
+        cout << endl
+             << "Default constructor called" << endl;
         name = "0";
         age = 0;
         salary = 0;
     }
 
-    worker(string n,int a,int s) {
-        cout<<endl<<"Parameterized constructor called"<<endl;
+    worker(string n, int a, int s)
+    {
+        cout << endl
+             << "Parameterized constructor called" << endl;
         name = n;
         age = a;
         salary = s;
     }
 
-    worker(worker &a) {
-        cout<<endl<<"Copy constructor called"<<endl;
+    worker(worker &a)
+    {
+        cout << endl
+             << "Copy constructor called" << endl;
         name = a.name;
         age = a.age;
         salary = a.salary;
     }
 
-    ~worker() {
-        cout<<endl<<"Destructor called";
+    ~worker()
+    {
+        cout << endl
+             << "Destructor called";
     }
 
-    void printinfo() {
-        cout<<endl;
-        cout<<"Name is : "<<name<<endl;
-        cout<<"Age is : "<<age<<endl;
-        cout<<"Salary is : "<<salary<<endl;
+    void printinfo()
+    {
+        cout << endl;
+        cout << "Name is : " << name << endl;
+        cout << "Age is : " << age << endl;
+        cout << "Salary is : " << salary << endl;
     }
-
 };
-int main ()
+int main()
 {
-    worker a("hamza",23,21000);
+    worker a("hamza", 23, 21000);
     worker b;
     worker c = a;
     worker d = a;
     worker e = b;
-    
+
     a.printinfo();
     b.printinfo();
     c.printinfo();
---
+++ this_keyword.cpp
@@ -0,0 +1,6 @@
+// In C++, the "this" keyword refers to a pointer that holds the memory address of the current object
+// instance within a member function of that object's class. It allows you to access members and
+// methods of the current object within its own methods. "this" is particularly useful when you have
+// local variables or parameters with the same names as member variables, as it helps differentiate
+// between them. By using "this", you can explicitly specify that you are referring to the member
+// variables of the current object.
